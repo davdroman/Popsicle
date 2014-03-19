4de0130bb943743b18fd304522cfa3ed77327d8d
Sam Spencer <spencer.samuel1@gmail.com>
2014-03-18 21:20:19 -0400

    Converted to Storyboards
    Changed code to support storyboard initialization. Clean-up some settings, images, and code.

--- Demo/DRDynamicSlideShow/MainViewController.h
+++ Demo/DRDynamicSlideShow/MainViewController.h
@@ -10,10 +10,22 @@
 
 #import "DRDynamicSlideShow.h"
 
-@interface MainViewController : UIViewController
+@interface MainViewController : UIViewController <UIBarPositioningDelegate>
 
-@property (strong, nonatomic) UINavigationBar * navigationBar;
-@property (strong, nonatomic) DRDynamicSlideShow * slideShow;
-@property (strong, nonatomic) NSArray * viewsForPages;
+// Slide Show
+@property (weak, nonatomic) IBOutlet DRDynamicSlideShow *slideShow;
+@property (weak, nonatomic) IBOutlet UIPageControl *pageControl;
+
+// Page 0
+@property (weak, nonatomic) IBOutlet UILabel *helloLabel;
+@property (weak, nonatomic) IBOutlet UITextView *welcomeLabel;
+
+// Page 1
+@property (weak, nonatomic) IBOutlet UIImageView *magicStickImageView;
+@property (weak, nonatomic) IBOutlet UITextView *magicStickDescriptionTextView;
+
+// Page 2
+@property (weak, nonatomic) IBOutlet UILabel *codeBracketsLabel;
+@property (weak, nonatomic) IBOutlet UITextView *codingDescriptionTextView;
 
 @end
