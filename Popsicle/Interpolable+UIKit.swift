43da722c2a2834319d3c96efc8a979aa4140ebeb
davdroman <d@vidroman.dev>
2016-07-17 04:10:56 +0200

    Improve formatting for `Interpolable` + tests

--- Popsicle/Interpolable+UIKit.swift
+++ Popsicle/Interpolable+UIKit.swift
@@ -18,7 +18,7 @@ extension CGPoint: Interpolable {
 	public static func interpolate(from fromValue: CGPoint, to toValue: CGPoint, at time: Time) -> CGPoint {
 		let x = CGFloat.interpolate(from: fromValue.x, to: toValue.x, at: time)
 		let y = CGFloat.interpolate(from: fromValue.y, to: toValue.y, at: time)
-		
+
 		return CGPoint(x: x, y: y)
 	}
 }
@@ -27,7 +27,7 @@ extension CGSize: Interpolable {
 	public static func interpolate(from fromValue: CGSize, to toValue: CGSize, at time: Time) -> CGSize {
 		let width = CGFloat.interpolate(from: fromValue.width, to: toValue.width, at: time)
 		let height = CGFloat.interpolate(from: fromValue.height, to: toValue.height, at: time)
-		
+
 		return CGSize(width: width, height: height)
 	}
 }
@@ -36,7 +36,7 @@ extension CGRect: Interpolable {
 	public static func interpolate(from fromValue: CGRect, to toValue: CGRect, at time: Time) -> CGRect {
 		let origin = CGPoint.interpolate(from: fromValue.origin, to: toValue.origin, at: time)
 		let size = CGSize.interpolate(from: fromValue.size, to: toValue.size, at: time)
-		
+
 		return CGRect(x: origin.x, y: origin.y, width: size.width, height: size.height)
 	}
 }
@@ -58,12 +58,12 @@ extension CGAffineTransform: Interpolable {
 	public static func interpolate(from fromValue: CGAffineTransform, to toValue: CGAffineTransform, at time: Time) -> CGAffineTransform {
 		let tx = CGFloat.interpolate(from: fromValue.translationX, to: toValue.translationX, at: time)
 		let ty = CGFloat.interpolate(from: fromValue.translationY, to: toValue.translationY, at: time)
-		
+
 		let sx = CGFloat.interpolate(from: fromValue.scaleX, to: toValue.scaleX, at: time)
 		let sy = CGFloat.interpolate(from: fromValue.scaleY, to: toValue.scaleY, at: time)
-		
+
 		let angle = CGFloat.interpolate(from: fromValue.angle, to: toValue.angle, at: time)
-		
+
 		return CGAffineTransform.identity.translateBy(x: tx, y: ty).scaleBy(x: sx, y: sy).rotate(angle.toRadians)
 	}
 }
@@ -74,20 +74,20 @@ extension UIColor: Interpolable {
 		var fromGreen = CGFloat()
 		var fromBlue = CGFloat()
 		var fromAlpha = CGFloat()
-		
+
 		var toRed = CGFloat()
 		var toGreen = CGFloat()
 		var toBlue = CGFloat()
 		var toAlpha = CGFloat()
-		
+
 		fromValue.getRed(&fromRed, green: &fromGreen, blue: &fromBlue, alpha: &fromAlpha)
 		toValue.getRed(&toRed, green: &toGreen, blue: &toBlue, alpha: &toAlpha)
-		
+
 		let red = CGFloat.interpolate(from: fromRed, to: toRed, at: time)
 		let green = CGFloat.interpolate(from: fromGreen, to: toGreen, at: time)
 		let blue = CGFloat.interpolate(from: fromBlue, to: toBlue, at: time)
 		let alpha = CGFloat.interpolate(from: fromAlpha, to: toAlpha, at: time)
-		
+
 		return UIColor(red: red, green: green, blue: blue, alpha: alpha)
 	}
 }
--- PopsicleTests/InterpolableTests.swift
+++ PopsicleTests/InterpolableTests.swift
@@ -11,7 +11,7 @@ import Popsicle
 
 class InterpolableTests: XCTestCase {
 
-	func test<T: Interpolable>(initialValue: T, finalValue: T, interpolatedValues: [T], times: [Time], function: (T, T) -> Bool) {
+	static func test<T: Interpolable>(initialValue: T, finalValue: T, interpolatedValues: [T], times: [Time], function: (T, T) -> Bool) {
 		for (index, time) in times.enumerated() {
 			let interpolatedValue = interpolatedValues[index]
 			let computedInterpolatedValue = T.interpolate(from: initialValue, to: finalValue, at: time)
@@ -19,67 +19,75 @@ class InterpolableTests: XCTestCase {
 		}
 	}
 
-	func testInterpolableBool() {
-		test(initialValue: false,
-		     finalValue: true,
-		     interpolatedValues: [false, false, false, true, true, true, true],
-		     times: [0, 0.25, 0.33, 0.5, 0.66, 0.75, 1],
-		     function: ==
+	func testInterpolableCGFloat() {
+		InterpolableTests.test(
+			initialValue: false,
+			finalValue: true,
+			interpolatedValues: [false, false, false, true, true, true, true],
+			times: [0, 0.25, 0.33, 0.5, 0.66, 0.75, 1],
+			function: ==
 		)
 
-		test(initialValue: true,
-		     finalValue: false,
-		     interpolatedValues: [true, true, true, false, false, false, false],
-		     times: [0, 0.25, 0.33, 0.5, 0.66, 0.75, 1],
-		     function: ==
+		InterpolableTests.test(
+			initialValue: true,
+			finalValue: false,
+			interpolatedValues: [true, true, true, false, false, false, false],
+			times: [0, 0.25, 0.33, 0.5, 0.66, 0.75, 1],
+			function: ==
 		)
 	}
 
 	func testInterpolableInt() {
-		test(initialValue: 0,
-		     finalValue: -10,
-		     interpolatedValues: [0, -2, -3, -5, -6, -7, -10],
-		     times: [0, 0.25, 0.33, 0.5, 0.66, 0.75, 1],
-		     function: ==
+		InterpolableTests.test(
+			initialValue: 0,
+			finalValue: -10,
+			interpolatedValues: [0, -2, -3, -5, -6, -7, -10],
+			times: [0, 0.25, 0.33, 0.5, 0.66, 0.75, 1],
+			function: ==
 		)
 
-		test(initialValue: 0,
-		     finalValue: 10,
-		     interpolatedValues: [0, 2, 3, 5, 6, 7, 10],
-		     times: [0, 0.25, 0.33, 0.5, 0.66, 0.75, 1],
-		     function: ==
+		InterpolableTests.test(
+			initialValue: 0,
+			finalValue: 10,
+			interpolatedValues: [0, 2, 3, 5, 6, 7, 10],
+			times: [0, 0.25, 0.33, 0.5, 0.66, 0.75, 1],
+			function: ==
 		)
 	}
 
 	func testInterpolableFloat() {
-		test(initialValue: Float(0),
-		     finalValue: -10,
-		     interpolatedValues: [0, -2.5, -3.3, -5, -6.6, -7.5, -10],
-		     times: [0, 0.25, 0.33, 0.5, 0.66, 0.75, 1],
-		     function: ~=
+		InterpolableTests.test(
+			initialValue: Float(0),
+			finalValue: -10,
+			interpolatedValues: [0, -2.5, -3.3, -5, -6.6, -7.5, -10],
+			times: [0, 0.25, 0.33, 0.5, 0.66, 0.75, 1],
+			function: ~=
 		)
 
-		test(initialValue: Float(0),
-		     finalValue: 10,
-		     interpolatedValues: [0, 2.5, 3.3, 5, 6.6, 7.5, 10],
-		     times: [0, 0.25, 0.33, 0.5, 0.66, 0.75, 1],
-		     function: ~=
+		InterpolableTests.test(
+			initialValue: Float(0),
+			finalValue: 10,
+			interpolatedValues: [0, 2.5, 3.3, 5, 6.6, 7.5, 10],
+			times: [0, 0.25, 0.33, 0.5, 0.66, 0.75, 1],
+			function: ~=
 		)
 	}
 
 	func testInterpolableDouble() {
-		test(initialValue: 0.0,
-		     finalValue: -10,
-		     interpolatedValues: [0, -2.5, -3.3, -5, -6.6, -7.5, -10],
-		     times: [0, 0.25, 0.33, 0.5, 0.66, 0.75, 1],
-		     function: ~=
+		InterpolableTests.test(
+			initialValue: 0.0,
+			finalValue: -10,
+			interpolatedValues: [0, -2.5, -3.3, -5, -6.6, -7.5, -10],
+			times: [0, 0.25, 0.33, 0.5, 0.66, 0.75, 1],
+			function: ~=
 		)
 
-		test(initialValue: 0.0,
-		     finalValue: 10,
-		     interpolatedValues: [0, 2.5, 3.3, 5, 6.6, 7.5, 10],
-		     times: [0, 0.25, 0.33, 0.5, 0.66, 0.75, 1],
-		     function: ~=
+		InterpolableTests.test(
+			initialValue: 0.0,
+			finalValue: 10,
+			interpolatedValues: [0, 2.5, 3.3, 5, 6.6, 7.5, 10],
+			times: [0, 0.25, 0.33, 0.5, 0.66, 0.75, 1],
+			function: ~=
 		)
 	}
 }
