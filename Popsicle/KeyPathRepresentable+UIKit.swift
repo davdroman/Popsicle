2313df74460df51992fcee609d59b855c593811a
davdroman <d@vidroman.dev>
2016-07-17 19:38:33 +0200

    Allow cascade bindings

--- Popsicle/KeyPathRepresentable+UIKit.swift
+++ Popsicle/KeyPathRepresentable+UIKit.swift
@@ -12,21 +12,18 @@ extension NSLayoutAttribute: KeyPathRepresentable {
 	public func object(from originalObject: NSObject) -> NSObject {
 		if let view = originalObject as? UIView, let superview = view.superview {
 			let constrainedView = (self == .width || self == .height) ? view : superview
-			
+
 			for constraint in constrainedView.constraints where
-				
 				!constraint.isKind(of: NSClassFromString("NSContentSizeLayoutConstraint")!) &&
-					((constraint.firstItem as? NSObject == view && constraint.firstAttribute == self) ||
-						(constraint.secondItem as? NSObject == view && constraint.secondAttribute == self))
-				
-			{
+				((constraint.firstItem as? NSObject == view && constraint.firstAttribute == self) ||
+				(constraint.secondItem as? NSObject == view && constraint.secondAttribute == self)) {
 				return constraint
 			}
 		}
-		
+
 		fatalError("Object \(originalObject) is not a UIView or is not constrained to a superview")
 	}
-	
+
 	public func keyPath() -> String {
 		return #keyPath(NSLayoutConstraint.constant)
 	}
