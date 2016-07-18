85e638854a19d7a345915215f9cbbb8144bd41b2
davdroman <d@vidroman.dev>
2016-07-18 13:05:18 +0200

    Remove trailing whitespaces to conform to linter rules

--- PopsicleTests/InterpolationTests.swift
+++ PopsicleTests/InterpolationTests.swift
@@ -15,23 +15,23 @@ class InterpolationTests: XCTestCase {
 		let view = UIView()
 		let interpolation1 = Interpolation(view, alpha)
 		let interpolation2 = Interpolation(view, alpha)
-		
+
 		XCTAssert(interpolation1 == interpolation2, "\(interpolation1) != \(interpolation2)")
 	}
-	
+
 	func testInequality() {
 		let interpolation1 = Interpolation(UIView(), alpha)
 		let interpolation2 = Interpolation(UIView(), alpha)
-		
+
 		XCTAssert(interpolation1 != interpolation2, "\(interpolation1) == \(interpolation2)")
 	}
-	
+
 	func testPolesOrder() {
 		let interpolation = Interpolation(UIView(), alpha)
 		interpolation[1] = 25
 		interpolation[2] = 50
 		interpolation[0] = 10
-		
+
 		XCTAssert(interpolation[0] == 10, "\(interpolation[0]) != 10")
 		XCTAssert(interpolation[0.5] == 17.5, "\(interpolation[0.5]) != 17.5")
 		XCTAssert(interpolation[1] == 25, "\(interpolation[1]) != 25")
