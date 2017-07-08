e21977b57d4840a4a457ef7d5ab30490a56286ac
davdroman <d@vidroman.dev>
2017-07-08 01:37:52 +0100

    Add demo target

---
+++ Demo/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  PopsicleDemo
+//
+//  Created by David Roman on 08/07/2017.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
