6d3847d5d83db0fc5068fc9f928ef35f8d9b9f8f
asos-davdroman <david.roman@asos.com>
2018-07-27 14:16:23 +0100

    Gardening

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -6,19 +6,12 @@
 //
 
 import UIKit
+import Popsicle
 
 class ViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
 	}
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
-	}
-
-
 }
 
